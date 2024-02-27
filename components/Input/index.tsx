7932ae66ad51909db44a3946a42ee6076adf91dc
varungupta2906 <varun.gupta@primathon.in>
2024-02-27 23:39:54 +0530

    Basic UI Setup

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,14 +1,17 @@
-/* eslint-disable react/no-array-index-key */
+/* eslint-disable */
 // @ts-nocheck
 import React, { useState } from 'react'
 import RightArrow from "../../public/images/svgs/right-arrow.svg"
 import Mic from "../../public/images/svgs/microphone.svg";
+import BotIcon from "../../public/images/svgs/purple-icon.svg";
 
 import styles from "./styles.module.scss"
 
 const Input = () => {
     const [messages, setMessages] = useState<string[]>([]);
     const [inputValue, setInputValue] = useState('');
+    const daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+    const date = new Date()
 
     const handleInputChange = (event: any) => {
         setInputValue(event.target.value);
@@ -31,17 +34,31 @@ const Input = () => {
                 sender: 'bot',
             };
             setMessages([...messages, botResponse]);
-        }, 500);
+        }, 1000);
     };
 
     return (
         <div>
-            <div>
-                {messages.map((message: any, index: any) => (
-                    <div key={index} className={`message ${message.sender}`}>
-                        {message.text}
-                    </div>
-                ))}
+            <div className={styles.chat}>
+                <p className={styles.date}>
+                    {messages.length > 0 && `${daysOfWeek[date.getDay()]} ${date.toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true })}`}
+                </p>
+                <div>
+                    {messages.map((message: any, index: any) => (
+                        <div>
+                            {<div key={index} className={styles.user}>
+                                {message.text}
+                            </div>}
+                            {message.sender === "bot" &&
+                                <div className={styles.messageContainer}>
+                                    <BotIcon />
+                                    <div key={index} className={styles.bot}>
+                                        {message.text}
+                                    </div>
+                                </div>}
+                        </div>
+                    ))}
+                </div>
             </div>
             <div className={styles.inputContainer}>
                 <div className={styles.inputMicContainer}>
@@ -52,7 +69,7 @@ const Input = () => {
                         onChange={handleInputChange}
                         className={styles.input}
                     />
-                    <Mic width={24} height={24} />
+                    <Mic width={24} height={24} style={{ cursor: "pointer" }} />
                 </div>
                 <button type="button" onClick={handleSendMessage} className={styles.button}>
                     <RightArrow width={24} height={24} />
