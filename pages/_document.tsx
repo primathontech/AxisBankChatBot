483847f8fa32a8457111c167dbb43a62e36676da
varungupta2906 <varun.gupta@primathon.in>
2024-02-29 21:14:15 +0530

    typing animation added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter:wght@100..900&display=swap&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+                    <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
                 </Head>
                 <body>
                     <Main />
